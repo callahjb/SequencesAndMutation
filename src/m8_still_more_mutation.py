1a3dcc2fdd22c0c3cb06b202752b9200ec6e484f
callahjb <callahjb@rose-hulman.edu>
2018-01-17 21:40:26 -0500

    hello

--- src/m8_still_more_mutation.py
+++ src/m8_still_more_mutation.py
@@ -4,8 +4,8 @@ In this module, you mutate by DELETING elements of a list.
 
 Authors: David Mutchler, Amanda Stouder, Chandan Rupakheti, Katie Dion,
          Claude Anderson, Delvin Defoe, Curt Clifton, their colleagues,
-         and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         and joseph callahna.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import m6_mutation
 
@@ -65,13 +65,20 @@ def RETURN_delete_negatives(numbers):
       :type numbers: list
     where the list is a list of numbers.
     """
-    # TODO: 2. First, READ THE ABOVE TEST CODE.
+    # done: 2. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          In particular, note how it calls the   run_test   function
     #          from the module   m6_mutation   by using the notation:
     #             m6_mutation.run_test(...)
     #          Then, IMPLEMENT and test THIS FUNCTION
     #          (using the above code for testing).
+
+    hello = []
+    for k in range(len(numbers)):
+        if numbers[k] >= 0:
+            hello += [numbers[k]]
+
+    return hello
 
 
 def run_test_MUTATE_delete_negatives():
@@ -124,7 +131,7 @@ def MUTATE_delete_negatives(numbers):
 
     Precondition: The argument is a list of numbers.
     """
-    # TODO: 3. First, READ THE ABOVE TEST CODE.
+    # done: 3. First, READ THE ABOVE TEST CODE.
     #          Make sure that you understand it.
     #          In particular, note how it calls the   run_test   function
     #          from the module   m6_mutation   by using the notation:
@@ -137,6 +144,12 @@ def MUTATE_delete_negatives(numbers):
     #       Take a stab at this problem,
     #       then see the solutions posted on Piazza.
     #       See those solutions even if your approach passes the tests.
+
+    for k in range(len(numbers)-1,-1,-1):
+        if numbers[k] < 0:
+            del numbers[k]
+
+    return numbers
 
 
 # ----------------------------------------------------------------------
